aa3dc6bf8be09601196e92e188df10044eb843f7
alex.polak <alex.polak@hulu.com>
2019-02-01 15:44:30 -0800

    support for pulling commit data for commits specified in https://docs.google.com/spreadsheets/d/1WgyAUhB9hOF4hYWhF4x5Y0P09ca5Mul958lPLfvVtQg/edit#gid=1455948947

---
+++ squaad/commit_msg_consistency/commit_msg_collection.py
@@ -0,0 +1,43 @@
+from squaad import db_lite
+from dataclasses import dataclass
+from datetime import datetime
+from squaad.git_local import GitRepo, GitData
+import pandas as pd
+
+
+def collect_commit_data(name):
+
+	df_apps = pd.read_csv(name, usecols=["Project Name", "Commit ID"])
+	db = db_lite.db('esem_db', 'alex.polak')
+
+	apps = db.table_to_pd('applications', cols=['repo_url', 'application'], index_col='application')
+
+	commits_by_app = df_apps.groupby(by="Project Name")
+
+	stats = []
+	for app, app_commits in commits_by_app:
+		print(app)
+		app = app.strip()
+		repo_url = apps.loc[app].repo_url
+		git_repo = GitRepo()
+		git_repo.load_repo(repo_url=repo_url)
+		git_data = GitData(git_repo.repo)
+
+		for commit in app_commits["Commit ID"]:
+			commit_data = git_data.get_commit_stats(commit)
+			stats.append([app, commit, commit_data.commit_msg,
+							commit_data.overall_stats['insertions'],
+					   		commit_data.overall_stats['deletions'],
+					   		commit_data.overall_stats['lines'],
+					   		commit_data.overall_stats['files'],
+					   		", ".join(list(commit_data.file_stats.keys()))])
+
+	df = pd.DataFrame(stats, columns=['app', 'commit', 'msg', 'insertions', 'deletions', 'lines', 'files', 'file_names'])
+
+	df.to_csv("new_" + name)
+	print("data saved")
+
+if __name__ == '__main__':
+	for name in ["~/Downloads/Consistency Between Commit Message and Content - Neutral.csv",
+				 "~/Downloads/Consistency Between Commit Message and Content - Breaker.csv"]:
+		collect_commit_data(name)
