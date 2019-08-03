b3f42c92d5295457b66b0a9ffc84fbd08f9caed5
Iordanis Fostiropoulos <iordanis.gpueater@gmail.com>
2019-08-03 14:11:49 +0900

    Fix prereqs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="squaad",
-    version="2.0",
+    version="2.1",
     author='Iordanis Fostiropoulos',
     author_email='danny.fostiropoulos@gmail.com',
     description='Helper functions for running queries, ml pipeline, statistical analysis on SQUAAD framework',
@@ -16,7 +16,21 @@ setuptools.setup(
     package_data={
    'squaad': ['sql/*']
 
-   },
+   },install_requires=[
+    'psycopg2-binary',
+    'xlwt',
+    'GitPython',
+    'SQLAlchemy',
+    'imbalanced-learn',
+    'imblearn',
+    'matplotlib',
+    'numpy',
+    'pandas',
+    'python-dateutil',
+    'scipy',
+    'seaborn',
+    'sklearn'
+   ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
